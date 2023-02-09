be67bf32c854bdf06befa74efba0c71fc7b25b95
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-02-09 22:30:06 +0300

    add ping pong

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type album struct {
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
+
 // albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
@@ -21,11 +22,16 @@ var albums = []album{
 
 func main() {
 	router := gin.Default()
+	router.GET("/ping", ping)
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 
 	router.Run("localhost:8080")
+}
+
+func ping(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "pong"})
 }
 
 // getAlbums responds with the list of all albums as JSON.
@@ -55,11 +61,3 @@ func getAlbumByID(c *gin.Context) {
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
-
-
-
-
-
-
-
-
